b1fd851790441321dae95951d5414692e18641cb
yushamariak <yushamariak@gmail.com>
2017-12-10 00:54:40 -0200

    Servlet to add number with validations

---
+++ Exercise2/src/main/java/com/mycompany/exercise2/Add.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.exercise2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author yusha
+ */
+public class Add extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        try (PrintWriter out = response.getWriter()) {
+            out.printf("<!DOCTYPE html>\n"
+                    + "<html lang=\"pt-br\">\n"
+                    + "    <head>\n"
+                    + "        <title>Exercise 2 Servlets</title>\n"
+                    + "        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n"
+                    + "    </head>\n"
+                    + "    <body>\n");
+                double x = Double.parseDouble(request.getParameter("x"));
+                double y = Double.parseDouble(request.getParameter("y"));
+                out.printf("    <p>%.2f + %.2f = %.2f</p>\n", x, y, (x + y));
+                out.printf("<form action=\"/Exercise2/\" method=\"post\">\n"
+                    + "     <input type=\"submit\"  value=\"Back\">\n"
+                    + "</form>");
+            out.printf("    </body>\n</html>");
+        }
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
